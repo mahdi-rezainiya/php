9f81c107672e2d108d41dbc1e022fe844f93f4e0
mahdi <mahdi.rezainiya@gmail.com>
2024-02-21 23:14:25 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+    // $name = "mahdi";
+    // echo $name;
+    // define('NAME' , 'mahdi');
+    // echo NAME;
+
+    // NAME = 'ali';
+    // echo NAME;
+
+    // define('NAME' , 'ali');
+    // echo NAME;
+    // string
+    $firstName = "mahdi";
+    $lastName = "rezainiya";
+    // echo $firstName.$lastName;
+    // echo "Hi ".$firstName;
+    
+    // echo 'Hi $firstName';
+    // echo "Hi $firstName $lastName";
+
+    // echo strlen($firstName);
+    // echo strtolower($firstName);
+    // echo strtoupper($firstName);
+
+    echo str_replace('m' , 'n' , $firstName);
+
+?>
+
+
+<!-- <!DOCTYPE html> -->
+<!-- <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Project</title>
+</head>
+<body> -->
+
+<?php
+    // echo "Hello World!";
+?>
+
+<!-- <br> -->
+
+<!-- <button> -->
+    <?php
+        // echo "click me";
+    ?>
+<!-- </button> -->
+
+<!-- </body>
+</html> -->
