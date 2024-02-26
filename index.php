7880f371d6b15acd6c9cc9dd02f5482d5dabbfa8
mahdi_rezainiya <95291203+mahdi-rezainiya@users.noreply.github.com>
2024-02-26 15:16:55 +0330

    these files are for develop branch

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+	// include 'Templates/header.php';
+	// include 'Templates/footer.php';
+	include 'add.php';
+?>
+
+</html>
---
+++ names.php
@@ -0,0 +1,4 @@
+<?php 
+    $names = ["mahdi" , "bahram" , "atefe" , "nader"];
+    echo $names[0] . '<br>';
+?>
