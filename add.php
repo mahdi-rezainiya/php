681ef9b555eed210fa93214c079beb9ea3fb21ec
mahdi <mahdi.rezainiya@gmail.com>
2024-04-04 09:57:43 +0330

    new changes

--- add.php
+++ add.php
@@ -6,7 +6,11 @@
 			echo 'Email Required';
 			echo '<br>';
 		}else{
-			echo htmlspecialchars($_POST['email']);
+			// echo htmlspecialchars($_POST['email']);
+			$email = $_POST['email'];
+			if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+				echo 'Email is not Valid';
+			}
 			echo '<br>';
 		}
 		// Title Validation
@@ -14,76 +18,25 @@
 			echo 'Title Required';
 			echo '<br>';
 		}else{
-			echo htmlspecialchars($_POST['title']);
+			$title = $_POST['title'];
+			// echo htmlspecialchars($_POST['title']);
+			if(!preg_match('/^[a-zA-Z\s]+$/',$title)){
+				echo 'Title must be letters and spaces only';
+			}
 			echo '<br>';
 		}
 		// Ingredients Validation
 		if(empty($_POST['ingredients'])){
 			echo 'Ingredients Required';
-			echo '<br>';
 		}else{
-			echo htmlspecialchars($_POST['ingredients']);
+			// echo htmlspecialchars($_POST['ingredients']);
+			$ingredients = $_POST['ingredients'];
+			if(!preg_match('/^[a-zA-Z\s]+(,\s?[a-zA-Z\s]*)*$/' , $ingredients)){
+				echo 'Ingredients should Separated by Comma ';
+			}
 			echo '<br>';
 		}
 	}
-
-	
-
-
-
-
-	
-	// Email validation
-	// if(empty($_POST['email'])){
-	// 	echo 'Email Required <br>';
-	// } else{
-	// 	echo htmlspecialchars($_POST['email']);
-	// 	echo '<br>';
-	// }
-	// // Title validation
-	// if(empty($_POST['title'])){
-	// 	echo 'Title Required <br>';
-	// } else{
-	// 	echo htmlspecialchars($_POST['title']);
-	// 	echo '<br>';
-	// }
-	// // Ingredients validation
-	// if(empty($_POST['ingredients'])){
-	// 	echo 'Ingredients Required <br>';
-	// } else{
-	// 	echo htmlspecialchars($_POST['ingredients']);
-	// 	echo '<br>';
-	// }
-	
-	// if(empty($_POST['email'])){
-	// 	echo 'Email Required <br>';
-	// } else {
-	// 	$email = $_POST['email'];
-	// 	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-	// 		echo 'Email is not Valid <br>';
-	// 	}
-		// echo '<br>';
-	// }
-	// Title validation
-	// if(empty($_POST['title'])){
-	// 	echo 'Title Required <br>';
-	// } else {
-	// 	$title = $_POST['title'];
-	// 	if(!preg_match('/^[a-zA-Z\s]+$/',$title)){
-	// 		echo 'Title must be letters and spaces only<br>';
-	// 	}
-	// }
-	// Ingredients validation
-// 	if(empty($_POST['ingredients'])){
-// 		echo 'Ingredients Required <br>';
-// 	} else {
-// 		$ingredients = $_POST['ingredients'];
-// 		if(!preg_match('/^[a-zA-Z\s]+(,\s?[a-zA-Z\s]*)*$/' , $ingredients)){
-// 			echo 'Ingredients should Separated by Comma <br>';
-// 		}
-// 	}
-// }
-
 
 ?>
 
